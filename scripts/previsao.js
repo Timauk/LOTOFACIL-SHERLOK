f0a17983d1f220721776d2937de746b09deb5fa2
Tiago Vargas <timauk@hotmaill.com>
2024-10-10 12:07:46 -0300

    versão 1.0  de

--- scripts/previsao.js
+++
@@ -1,4 +0,0 @@
-function mostrarResultados() {
-    const resultadosDiv = document.getElementById("resultadosCiclo");
-    resultadosDiv.innerHTML = palpites.map(palpite => `<p>${palpite.join(", ")}</p>`).join("");
-}
